e8b92e60c51ffcefd49b4942dfd760d69592202d
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-08-05 10:57:50 -0700

    testimonials in a good place

--- wp-content/themes/123_one/page-testimonials.php
+++ wp-content/themes/123_one/page-testimonials.php
@@ -12,22 +12,29 @@
 			<div class="testimonials-testimonials-grid">
 			<?php while(have_rows('testimonials-repeater', 'option')): the_row(); ?>
 				<?php 
-				$select = get_sub_field('testimonials-repeater-select', 'option');
+				$select = get_sub_field(' testimonials-repeater-select', 'option');
 				$grid_item_class = $select == 'youtube' ? 'testimonials-youtubegriditem' : ''; 
+				if(!empty(get_sub_field('testimonials-repeater-image'))){
+					$grid_item_class .= ' hasimage';
+				}
 				?>
-				<div class="testimonials-testimonials-grid-item <?php echo $grid_item_class; ?>">
+				<div class="fade fade-up testimonials-testimonials-grid-item<?php echo $grid_item_class; ?>">
 					<?php if( get_sub_field('testimonials-repeater-select', 'option') == 'personal' ): ?>
-						<div class="testimonials-testimonials-grid-item-quote">“<?php echo get_sub_field('testimonials-repeater-quote'); ?>”</div>
-						<div class="testimonials-testimonials-grid-item-personinfo">
-							<div class="testimonials-testimonials-grid-item-personinfo-name">- <?php echo get_sub_field('testimonials-repeater-name'); ?></div>
+						<?php if(!empty(get_sub_field('testimonials-repeater-image'))): ?>
+							<div style="background-image: url('<?php echo get_sub_field('testimonials-repeater-image') ?>');" class="testimonials-testimonials-grid-item-image"></div>
+						<?php endif; ?>
+						<div class="testimonials-testimonials-grid-item-textwrap">	
+							<div class="testimonials-testimonials-grid-item-quote">“<?php echo get_sub_field('testimonials-repeater-quote'); ?>”</div>
+							<div class="testimonials-testimonials-grid-item-personinfo">
+								<div class="testimonials-testimonials-grid-item-personinfo-name">- <?php echo get_sub_field('testimonials-repeater-name'); ?></div>
+							</div>
 						</div>
-						<img src="<?php echo get_template_directory_uri(); ?>/library/img/home/testimonials/quotemark.png" class="testimonials-testimonials-grid-item-quotemark">
-						<?php if( !empty(get_sub_field('testimonials-repeater-image')) ): ?>
-							<img src="<?php echo get_sub_field('testimonials-repeater-image') ?>" class="testimonials-testimonials-grid-item-image">
-						<?php endif ?>
 					<?php endif; ?>
 					<?php if( get_sub_field('testimonials-repeater-select', 'option') == 'youtube' ): ?>
-						<div class="testimonials-testimonials-grid-item-youtubecontainer"><iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo get_sub_field('testimonials-repeater-youtube', 'option'); ?>" frameborder="0" allowfullscreen></iframe></div>
+						<div class="testimonials-testimonials-grid-item-youtubecontainer">
+							<?php the_sub_field('testimonials-repeater-youtube', 'option'); ?>
+							<!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo get_sub_field('testimonials-repeater-youtube', 'option'); ?>" frameborder="0" allowfullscreen></iframe> -->
+						</div>
 					<?php endif; ?>
 
 				</div>
