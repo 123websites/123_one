b34c1a7939f433f7f715576c32002a2de583105a
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-08-05 12:43:56 -0700

    fixed disable site not working - was a bug in the parent theme

--- wp-content/themes/123_one/footer.php
+++ wp-content/themes/123_one/footer.php
@@ -1,7 +1,9 @@
 <?php
-	get_template_part('partials/global', 'popups');
-	get_template_part('partials/navigation/footer', 'mobile');
-	get_template_part('partials/navigation/footer', 'desktop');
+	if( !get_field('disable-site', 'option') ){
+		get_template_part('partials/global', 'popups');
+		get_template_part('partials/navigation/footer', 'mobile');
+		get_template_part('partials/navigation/footer', 'desktop');
+	}
 
 	if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
 	   ?> <script src="//localhost:35729/livereload.js"></script> <?php
