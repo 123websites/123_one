a7933077f9ff4da9b21b711fa58111e8fd15c20d
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-09-17 09:50:13 -0700

    updated areas served to support countries served

--- wp-content/themes/123_one/page-areas-served.php
+++ wp-content/themes/123_one/page-areas-served.php
@@ -9,39 +9,52 @@
 		<div class="areas-served-hero-map" id="map"></div>
 	</section>
 	<section class="areas-served-areas">
-		<?php if(have_rows('locations', 'option')) : ?>
-			<div class="areas-served-areas-grid">
-				<?php 
-				$rows = get_field('locations', 'option'); 
-				$medium_remainder = count($rows) % 2;
-				$large_remainder = count($rows) % 4;
-				foreach($rows as $index => $row): 
-					$classes = '';
-					if(!empty($medium_remainder) && $index >= count($rows) - $medium_remainder){
-						$classes .= 'medium-' . (string) $medium_remainder . ' ';
-					}
-					if(!empty($large_remainder) && $index >= count($rows) - $large_remainder){
-						$classes .= 'large-' . (string) $large_remainder . ' ';
-					}
-					?>
-					<?php 
+		<div class="areas-served-areas-grid">
+			<?php 
+			$rows = [];
+			if( get_field('zips_or_countries', 'option') == 'zips' ){
+				if(have_rows('locations', 'option')) :
+					$rows = get_field('locations', 'option'); 
+					foreach($rows as $index => $row): 
 						$ch = curl_init();
-						curl_setopt($ch, CURLOPT_URL, 'http://maps.googleapis.com/maps/api/geocode/xml?address=' . $row['zip'] . '@&sensor=true');
+						curl_setopt($ch, CURLOPT_URL, 'http://maps.googleapis.com/maps/api/geocode/xml?latlng=' . $row['zip']['lat'] . ',' . $row['zip']['lng'] . '&sensor=true');
 						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 						$curl_return = curl_exec($ch);
 						$contents = simplexml_load_string($curl_return);
 					?>
-					<a href="https://www.google.com/maps/@<?php echo $contents->result->geometry->location->lat . ',' . $contents->result->geometry->location->lng . ',14z'; ?>" class="areas-served-areas-grid-imagecontainer<?php echo !empty($classes) ? ' ' . $classes : '';  ?>" target="_blank">
-						<div class="areas-served-areas-grid-imagecontainer-citystate"><?php 
-							preg_match_all('/^.*?(?=(\d))/', $contents->result->formatted_address, $preg_match_all_matches);
-							echo $preg_match_all_matches[0][0];
-						?></div>
-						<div class="areas-served-areas-grid-imagecontainer-tint"></div>
-						<div style="background-image: url('<?php echo $row['area-image']; ?>');" class="areas-served-areas-grid-imagecontainer-image"></div>
-					</a>
-			<?php endforeach; ?>
-			</div>
-		<?php endif; ?>
+						<a href="https://www.google.com/maps/@<?php echo $contents->result->geometry->location->lat . ',' . $contents->result->geometry->location->lng . ',14z'; ?>" class="fade fade-up areas-served-areas-grid-imagecontainer" target="_blank">
+							<div style="background-image: url('<?php echo $row['area-image']; ?>');" class="areas-served-areas-grid-imagecontainer-image"></div>
+							<div class="areas-served-areas-grid-imagecontainer-citystate"><?php 
+								preg_match_all('/\d{5}/', $contents->result->formatted_address, $preg_match_all_matches);
+								$ch = curl_init();
+								curl_setopt($ch, CURLOPT_URL, 'http://maps.googleapis.com/maps/api/geocode/xml?address=' . $preg_match_all_matches[0][0] . '&sensor=true');
+								curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+								$curl_return = curl_exec($ch);
+								$contents = simplexml_load_string($curl_return);
+								preg_match_all('/^.*?(?=(\d))/', $contents->result->formatted_address, $preg_match_all_matches);
+								echo $preg_match_all_matches[0][0];
+							?></div>
+						</a>
+				<?php 
+					endforeach;
+				endif;
+			}
+			else{
+				$rows = get_field('countries', 'option');
+				if( have_rows('countries', 'optoin') ):
+					foreach($rows as $index => $row): 
+						
+					?>
+						<a href="<?php echo 'https://www.google.com/maps/search/' . rawurlencode($row['country']['label']) . '?hl=en&source=opensearch' ?>" class="fade fade-up areas-served-areas-grid-imagecontainer" target="_blank">
+							<div style="background-image: url('<?php echo $row['country_image']; ?>');" class="areas-served-areas-grid-imagecontainer-image"></div>
+							<div class="areas-served-areas-grid-imagecontainer-citystate"><?php echo $row['country']['label'] ?></div>
+						</a>
+				<?php 
+					endforeach;	
+				endif;
+			}
+			?>
+		</div>
 	</section>
 	<?php
 
