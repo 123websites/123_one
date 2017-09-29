7759d81c046e5e7ac56b37d980e4205fb48b1797
Zach Rubin-Rattet <zrubinrattet@gmail.com>
2017-09-28 17:36:30 -0700

    removed featured posts from contact page

--- wp-content/themes/123_one/page-contact.php
+++ wp-content/themes/123_one/page-contact.php
@@ -87,11 +87,6 @@
 			<?php echo do_shortcode('[gravityform id=1 title=false description=false ajax=true]'); ?>
 		</div>
 	</section>
-	<?php
-
-	get_template_part('partials/global', 'recent_posts');
-
-	?>
 </main>
 
 <?php get_footer(); ?>
